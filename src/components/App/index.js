c5dbebf817d171d2fd075b99f85024f7dc655298
Fabrice DENNEMONT <fabden@gmail.com>
2020-05-21 14:41:22 +0200

    debut du projet avec boostrarp et le background

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+
+import './styles.css';
+
+// == Composant
+const App = () => (
+  <div> fdfgfgf</div>
+);
+
+// == Export
+export default App;
