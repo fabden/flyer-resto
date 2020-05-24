62a5049c1771893f1ed1a0d4920d05fe837e2d40
Fabrice DENNEMONT <fabden@gmail.com>
2020-05-24 14:15:25 +0200

    positionement image pour ipad

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -20,58 +20,58 @@ const App = () => (
       <div className="fond_gris col-md-3 col-12 " />
       <div className="fond_vert col-md-5 col-12  d-none d-md-flex " />
     </div>
-    <div className="position-absolute ">
-    <header className="row justify-content-center text-center ">
-      <div className="col-md-3 order-md-2" />
-      <div className="col-md-9  order-md-1">
-        <h1 className="header_titre1 display-3">
+    <div className="position-absolute my-3 ">
+      <header className="row justify-content-center text-center ">
+        <div className="col-md-3 order-md-2" />
+        <div className="col-md-9  order-md-1">
+          <h1 className="header_titre1 display-3">
           Restaurant
-          <span className="row justify-content-center header_titre2 m-n2">
+            <span className="row justify-content-center header_titre2 m-n2">
           multiplats
-          </span>
-        </h1>
-        <p className="header_titre3 display-4 mb-5">
+            </span>
+          </h1>
+          <p className="header_titre3 display-4 mb-5">
           Choisi ton plat preferé
-        </p>
-      </div>
-    </header>
-    <div className="row justify-content-center align-items-end divImage pt-3  d-none d-md-flex position-relative mr-3">
-      <div className="col-md-8 text-right ">
-        <img className="img-fluid col-md-10 " src={imgPlat} />
-        <img className="col-md-10 position-absolute imgelemt img-fluid" src={imgelemt} />
-      </div>
-      <img className="img-fluid col-md-4 " src={imgChef} />
-    </div>
-    <div className="row justify-content-center col">
-      <div className="col-md-8 mt-5 ">
-        <div className="row justify-content-between text-wrap ">
-          <Item />
-          <Item />
+          </p>
         </div>
+      </header>
+      <div className="row justify-content-center align-items-end divImage pt-3  d-none d-md-flex position-relative mr-3">
+        <div className="col-md-8 text-right ">
+          <img className="img-fluid col-md-10 " src={imgPlat} />
+          <img className="col-md-10 position-absolute imgelemt img-fluid" src={imgelemt} />
+        </div>
+        <img className="img-fluid col-md-4 " src={imgChef} />
       </div>
-
-      <div className=" col-md-4">
-        <div className="row justify-content-center">
-          <div className="contact col-12">
-            <p className="text-center h2">Livraison<br /><span className="contact_span h2">05625622551555</span> </p>
+      <div className="row justify-content-center col">
+        <div className="col-md-8 mt-5 ">
+          <div className="row justify-content-around text-wrap ">
+            <Item />
+            <Item />
           </div>
-          <div className=" col-10 contact_header text-center text-wrap pt-3  d-none d-md-block">
-            <p className="h3">Commande en ligne <span className="text_psd">WWW.PSDFREEBIES.COM</span></p>
-            <div className="col-12 border-top pt-3 pw-3">
-              <p className="h5">Telecherger l'aplication <br /> Mobile</p>
-              <div className="row justify-content-center  py-3">
-                <div className="col-6 border-right ">
-                  <img className="img-fluid" src={iconeAndroid} />
-                </div>
-                <div className="col-6 ">
-                  <img className="img-fluid" src={iconeAppel} />
+        </div>
+
+        <div className=" col-md-4">
+          <div className="row justify-content-center">
+            <div className="contact col-12">
+              <p className="text-center h2">Livraison<br /><span className="contact_span h2">05625622551555</span> </p>
+            </div>
+            <div className=" col-10 contact_header text-center text-wrap pt-3  d-none d-md-block">
+              <p className="h3">Commande en ligne <span className="text_psd">WWW.PSDFREEBIES.COM</span></p>
+              <div className="col-12 border-top pt-3 pw-3">
+                <p className="h5">Telecherger l'aplication <br /> Mobile</p>
+                <div className="row justify-content-center  py-3">
+                  <div className="col-6 border-right ">
+                    <img className="img-fluid" src={iconeAndroid} />
+                  </div>
+                  <div className="col-6 ">
+                    <img className="img-fluid" src={iconeAppel} />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   </div>
 );
--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -3,18 +3,17 @@ import humberger from '../../assets/Images/humberger.png';
 
 
 const Item = () => (
-  <div className="col-6 ">
-    <div className="row justify-content-center mt-5">
-      <div className="col-9 text-right">
-        <span className=" divPromo p-3 align-top rounded-circle ">-20% </span>
-        <img className="col-9 mt-n3" src={humberger} />
+  <div className="col-md-5 my-2 border-left border-top">
+    <div className="row justify-content-center mt-5 ">
+      <div className="col-md-12 text-right">
+        <span className=" divPromo p-3 align-top rounded-circle position-absolute">-20% </span>
+        <img className="col-12 mt-n3" src={humberger} />
       </div>
-      <h1 className="col-9 text-center item_titre text-uppercase">Menu Hamberger</h1>
-      <p className="text-center col-9 item_descrition">letght
-  hgfhgfhgfhgfhgf
+      <p className="col-12 text-center item_titre text-uppercase h4">Menu Hamberger</p>
+      <p className="text-center col-10 item_descrition">tres bon hamburger a goûter
       </p>
-      <div className="col-4 item_prix_text text-center my-2"><p className="text-center py-2 h4">Prix</p></div>
-      <div className="col-4 item_prix_number py-2"><p className="text-center py-2 h4">12€</p></div>
+      <div className="col-6 item_prix_text text-center my-2"><p className="text-center py-2 h4">Prix</p></div>
+      <div className="col-6 item_prix_number py-2"><p className="text-center py-2 h4">12€</p></div>
     </div>
   </div>
 );
